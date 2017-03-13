2c513620976ecbb1fa9c200324783e605aa12089
Daniele Masotti <lele@imac-di-daniele.local>
2017-03-13 16:11:55 +0100

    initial commit

---
+++ GetPractice3/GetPractice3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetPractice3
+//
+//  Created by Daniele Masotti on 13.03.17.
+//  Copyright © 2017 Daniele Masotti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
